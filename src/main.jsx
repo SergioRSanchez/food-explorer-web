c837ed35b8b6a8ece6e7f2f5c30fd8da34bad279
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-02-03 09:51:32 -0300

    feat: testing swiper slides

--- src/main.jsx
+++ src/main.jsx
@@ -9,12 +9,13 @@ import theme from './styles/theme';
 import { Home } from './pages/Home/index';
 import { SignIn } from './pages/SignIn';
 import { SignUp } from './pages/SignUp';
+import { Teste } from './pages/Teste';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <Teste />
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/Teste/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 350px;
+
+  img {
+    height: 350px;
+    width: 100%;
+    object-fit: cover;
+  }
+`;
